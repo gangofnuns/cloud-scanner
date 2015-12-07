8e1b9519e9e411d5a41414185d9feab8390fdceb
Marc Slayton <marc@goabra.com>
2015-12-06 16:27:27 -0800

    first commit.

---
+++ willneverwork/willneverwork/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  willneverwork
+//
+//  Created by Marc Slayton on 12/6/15.
+//  Copyright © 2015 Marc Slayton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
